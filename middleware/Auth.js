56154901788016bc1a65e8001724366297a3b9e9
markuz899 <markuz.89@hotmail.it>
2023-12-26 22:14:57 +0100

    [feat] - global middleware

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -1,15 +1,12 @@
 // Auth.js
-async function fastifyMiddleware(fastify, options) {
-  fastify.addHook("preHandler", async (request, reply) => {
-    const auth = request.headers?.authorization;
-
-    if (auth === process.env.BASIC_AUTH_PASSWORD_PORTAL) {
-      return;
-    } else {
-      reply.code(401).send({ error: true, msg: "Access forbidden" });
-      return;
-    }
-  });
+async function authMiddleware(request, reply) {
+  const auth = request.headers?.authorization;
+  if (auth === process.env.BASIC_AUTH_PASSWORD) {
+    return;
+  } else {
+    reply.code(401).send({ error: true, msg: "Access forbidden" });
+    return;
+  }
 }
 
-module.exports = fastifyMiddleware;
+module.exports = authMiddleware;
